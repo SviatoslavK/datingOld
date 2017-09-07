cb43a4ed7a5c232fe10080c7e0d5ed3c44267e42
Slava <kolodaslav@gmail.com>
2017-09-07 15:49:03 +0300

    add scripts

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,4 @@
+// Functions for range input
 function getVals() {
     var parent = this.parentNode;
     var slides = parent.getElementsByTagName("input");
@@ -21,7 +22,7 @@ function getVals() {
     if (slide1 > slide2) { var tmp = slide2; slide2 = slide1; slide1 = tmp; }
 }
 
-
+// Paginations
 function customPaginate(items) {
     var paginateContainer = document.querySelector('.gallery__block');
     var itemsToPaginate = paginateContainer.querySelectorAll('.gallery__card');
@@ -63,6 +64,59 @@ function customPaginate(items) {
     })
 }
 
+function clearInput(e) {
+    var input = e.target.previousElementSibling
+    input.value = ''
+}
+
+var toTopBtn = document.querySelector('#btn__to-top');
+toTopBtn.addEventListener('click', function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+
+// Change status at card
+function setFavorite() {
+    var allCards = Array.from(document.querySelectorAll('.gallery__card'));
+    allCards.map(function (card) {
+        var favoriteText = card.querySelector('.button__favorite--text');
+        var favoriteBlock = card.querySelector('.card__button--favorite')
+        if (card.hasAttribute('favorite')) {
+            favoriteText.innerHTML = 'В избранных'
+        } else {
+            favoriteText.innerHTML = 'Избранное'
+        }
+
+        favoriteBlock.addEventListener('click', function () {
+            if (card.hasAttribute('favorite')) {
+                card.removeAttribute('favorite')
+                favoriteText.innerHTML = 'Избранное'
+            } else {
+                card.setAttribute('favorite', 'true')
+                favoriteText.innerHTML = 'В избранных'
+            }
+        })
+    })
+}
+
+var allSection = Array.from(document.querySelectorAll('.choose__item'));
+allSection.map(function (item) {
+    item.addEventListener('click', function () {
+        if (item.hasAttribute('checking')) {
+            item.removeAttribute('checking')
+        } else {
+            item.setAttribute('checking', 'true');
+            var rrr = allSection.filter(function(el) {
+                return el !== item
+            }).map(function(el) {
+                el.removeAttribute('checking')
+            })
+        }
+    })
+})
+
+window.onload = setFavorite()
 window.onload = customPaginate(8)
 window.onload = function () {
     var sliderSections = document.getElementsByClassName("range-slider");
@@ -77,16 +131,3 @@ window.onload = function () {
     }
 }
 
-function clearInput(e) {
-    var input = e.target.previousElementSibling
-    input.value = ''
-}
-
-
-var toTopBtn = document.querySelector('#btn__to-top');
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-toTopBtn.addEventListener('click', topFunction)
